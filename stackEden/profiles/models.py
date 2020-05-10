04fea0ad6c9da8fcac9c87b4934afb22f9840250
Sam Beans <samuelbeans92@gmail.com>
2020-05-10 18:15:46 -0500

    slight documentation +gitgnore

--- stackEden/profiles/models.py
+++ stackEden/profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+# list of job roles
 job_roles = [('Data Engineer', 'DATA ENGINEER'),
             ('Software Developer', 'SOFTWARE DEVELOPER'),
             ('Frontend developer', 'FRONTEND DEVELOPER'),
@@ -16,7 +17,19 @@ job_roles = [('Data Engineer', 'DATA ENGINEER'),
             ('IT Manager', 'IT MANAGER'),
             ('IT Director', 'IT DIRECTOR')
         ]
+
+
 class Job(models.Model):
+    """Set up the jobs data model
+
+    Attributes: 
+        uuid:   Primary key linking user to job posting 
+        titte:   Name of the jon
+        role_type:  Job category
+        posted_by:   Strung value of who job posting represents
+        date_opened:  Date job was posted
+        date_cosed:   Date job was taken down 
+    """
     uuid = models.IntegerField(primary_key=True,  default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100, editable=True)
     role_type = models.CharField(max_length=50,choices=job_roles)
--- stackEden/profiles/views.py
+++ stackEden/profiles/views.py
@@ -7,33 +7,41 @@ from .models import Job
 from rest_framework import status
 # Create your views here.
 
-class Jobs(APIView):
 
+class Jobs(APIView):
     def get(self, request):
+        """ Get all jobs record
+        """
         jobs = Job.objects.all()
         serializer =serializers.jobSerializer(jobs, many=True)
-        
         return Response(serializer.data)
 
+
     def post(self, request):
+        """ Post new jobs 
+        """
         serializer = serializers.jobSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save
-
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class JobsDetail(APIView):
 
+class JobsDetail(APIView):
     def get(self, request, uuid):
+        """ Get call for specific job posting
+        """
         try:
             job = Job.objects.get(pk = uuid)
         except Job.DoesNotExist:
             raise HTTP404
         serializer=serializers.jobSerializer(job)
         return Response(serializer.data)
-    
+
+
     def delete(self, request, uuid):
+        """Delete individual job
+        """
         try: 
             job=Job.objects.get(pk=uuid)
         except Job.DoesNotExist:
